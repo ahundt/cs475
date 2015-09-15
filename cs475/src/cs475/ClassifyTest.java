c12db0c2aaf6b735c6d94eaa195de1196eb613d4
Andrew Hundt <ATHundt@gmail.com>
2015-09-14 20:10:35 -0400

    classifyTest.java now also checks the nlp data

--- cs475/src/cs475/ClassifyTest.java
+++ cs475/src/cs475/ClassifyTest.java
@@ -13,39 +13,51 @@ public class ClassifyTest {
 	
    @Test
    public void trainAndTestBioMajority() throws IOException {
+	   System.out.println("\nbioMajority");
 Classify.main(new String[] {        "-mode", "train", "-algorithm", "majority", "-model_file", "bioOutputModelMajority", "-data", "./Homework 1 Data/bio/bio.train"});
 Classify.main(new String[] {        "-mode", "test", "-model_file", "bioOutputModelMajority", "-data", "./Homework 1 Data/bio/bio.dev", "-predictions_file", " bioPredictionsMajority"});
 }
    @Test
    public void trainAndTestFinanceMajority() throws IOException {
+	   System.out.println("\nfinanceMajority");
 Classify.main(new String[] {        "-mode", "train", "-algorithm", "majority", "-model_file", "financeOutputModelMajority", "-data", "./Homework 1 Data/finance/finance.train"});
 Classify.main(new String[] {        "-mode", "test", "-model_file", "financeOutputModelMajority", "-data", "./Homework 1 Data/finance/finance.dev", "-predictions_file", " financePredictionsMajority"});
 }
    @Test
    public void trainAndTestSpeechMajority() throws IOException {
+	   System.out.println("\nspeechMajority");
 //echo "**speech data**"
 Classify.main(new String[] {        "-mode", "train", "-algorithm", "majority", "-model_file", "speechOutputModelMajority", "-data", "./Homework 1 Data/speech/speech.train"});
 Classify.main(new String[] {        "-mode", "test", "-model_file", "speechOutputModelMajority", "-data", "./Homework 1 Data/speech/speech.dev", "-predictions_file", " speechPredictionsMajority"});
 }
    @Test
    public void trainAndTestSyntheticEasyMajority() throws IOException {
+	   System.out.println("\nsyntheticEasyMajority");
 //echo "**synthetic easy data**"
 Classify.main(new String[] {        "-mode", "train", "-algorithm", "majority", "-model_file", "syntheticEasyOutputModelMajority", "-data", "./Homework 1 Data/synthetic/easy.train"});
 Classify.main(new String[] {        "-mode", "test", "-model_file", "syntheticEasyOutputModelMajority", "-data", "./Homework 1 Data/synthetic/easy.dev", "-predictions_file", " syntheticEasyMajority"});
 }
    @Test
    public void trainAndTestSyntheticHardMajority() throws IOException {
+	   System.out.println("\nsyntheticHardMajority");
 //echo "**synthetic hard data**"
 Classify.main(new String[] {        "-mode", "train", "-algorithm", "majority", "-model_file", "syntheticHardOutputModelMajority", "-data", "./Homework 1 Data/synthetic/hard.train"});
 Classify.main(new String[] {        "-mode", "test", "-model_file", "syntheticHardOutputModelMajority", "-data", "./Homework 1 Data/synthetic/hard.dev", "-predictions_file", " syntheticHardMajority"});
 }
    @Test
    public void trainAndTestVisionMajority() throws IOException {
+	   System.out.println("\nvisionMajority");
 //echo "**vision data**"
 Classify.main(new String[] {        "-mode", "train", "-algorithm", "majority", "-model_file", "visionOutputModelMajority", "-data", "./Homework 1 Data/vision/vision.train"});
 Classify.main(new String[] {        "-mode", "test", "-model_file", "visionOutputModelMajority", "-data", "./Homework 1 Data/vision/vision.dev", "-predictions_file", " visionMajority"});
-
-
+   }
+   
+@Test
+public void trainAndTestNLPMajority() throws IOException {
+	   System.out.println("\nnlpMajority");
+//echo "**nlp data**"
+Classify.main(new String[] {        "-mode", "train", "-algorithm", "majority", "-model_file", "nlpOutputModelMajority", "-data", "./Homework 1 Data/nlp/nlp.train"});
+Classify.main(new String[] {        "-mode", "test", "-model_file", "nlpOutputModelMajority", "-data", "./Homework 1 Data/nlp/nlp.dev", "-predictions_file", " nlpMajority"});
 
 }
    
@@ -54,6 +66,7 @@ Classify.main(new String[] {        "-mode", "test", "-model_file", "visionOutpu
    
    @Test
    public void trainAndTestBioEvenOdd() throws IOException {
+	   System.out.println("\nbioEvenOdd");
 //echo "using even"-odd model"
 //echo "**bio data**"
 Classify.main(new String[] {        "-mode", "train", "-algorithm", "even_odd", "-model_file", "bioOutputModelEvenOdd", "-data", "./Homework 1 Data/bio/bio.train"});
@@ -61,33 +74,45 @@ Classify.main(new String[] {        "-mode", "test", "-model_file", "bioOutputMo
 }
    @Test
    public void trainAndTestFinanceEvenOdd() throws IOException {
+	   System.out.println("\nfinanceEvenOdd");
 //echo "**finance data**"
 Classify.main(new String[] {        "-mode", "train", "-algorithm", "even_odd", "-model_file", "financeOutputModelEvenOdd", "-data", "./Homework 1 Data/finance/finance.train"});
 Classify.main(new String[] {        "-mode", "test", "-model_file", "financeOutputModelEvenOdd", "-data", "./Homework 1 Data/finance/finance.dev", "-predictions_file", " financePredictionsEvenOdd"});
 }
    @Test
    public void trainAndTestSpeechEvenOdd() throws IOException {
+	   System.out.println("\nspeechEvenOdd");
 //echo "**speech data**"
 Classify.main(new String[] {            "-mode", "train", "-algorithm", "even_odd", "-model_file", "speechOutputModelEvenOdd", "-data", "./Homework 1 Data/speech/speech.train"});
 Classify.main(new String[] {        "-mode", "test", "-model_file", "speechOutputModelEvenOdd", "-data", "./Homework 1 Data/speech/speech.dev", "-predictions_file", " speechPredictionsEvenOdd"});
 }
    @Test
    public void trainAndTestSyntheticEasyEvenOdd() throws IOException {
+	   System.out.println("\nsyntheticEasyEvenOdd");
 //echo "**synthetic easy data**"
 Classify.main(new String[] {        "-mode", "train", "-algorithm", "even_odd", "-model_file", "syntheticEasyOutputModelEvenOdd", "-data", "./Homework 1 Data/synthetic/easy.train"});
 Classify.main(new String[] {        "-mode", "test", "-model_file", "syntheticEasyOutputModelEvenOdd", "-data", "./Homework 1 Data/synthetic/easy.dev", "-predictions_file", " syntheticEasyEvenOdd"});
 }
    @Test
    public void trainAndTestSyntheticHardEvenOdd() throws IOException {
+	   System.out.println("\nsyntheticHardEvenOdd");
 //echo "**synthetic hard data**"
 Classify.main(new String[] {        "-mode", "train", "-algorithm", "even_odd", "-model_file", "syntheticHardOutputModelEvenOdd", "-data", "./Homework 1 Data/synthetic/hard.train"});
 Classify.main(new String[] {        "-mode", "test", "-model_file", "syntheticHardOutputModelEvenOdd", "-data", "./Homework 1 Data/synthetic/hard.dev", "-predictions_file", " syntheticHardEvenOdd"});
 }
    @Test
    public void trainAndTestVisionEvenOdd() throws IOException {
+	   System.out.println("\nvisionEvenOdd");
 //echo "**vision data**"
 Classify.main(new String[] {        "-mode", "train", "-algorithm", "even_odd", "-model_file", "visionOutputModelEvenOdd", "-data", "./Homework 1 Data/vision/vision.train"});
 Classify.main(new String[] {        "-mode", "test", "-model_file", "visionOutputModelEvenOdd", "-data", "./Homework 1 Data/vision/vision.dev", "-predictions_file", " visionEvenOdd"});
+}
+   @Test
+   public void trainAndTestNLPEvenOdd() throws IOException {
+	   System.out.println("\nnlpEvenOdd");
+//echo "**nlp data**"
+Classify.main(new String[] {        "-mode", "train", "-algorithm", "even_odd", "-model_file", "nlpOutputModelEvenOdd", "-data", "./Homework 1 Data/nlp/nlp.train"});
+Classify.main(new String[] {        "-mode", "test", "-model_file", "nlpOutputModelEvenOdd", "-data", "./Homework 1 Data/nlp/nlp.dev", "-predictions_file", " nlpEvenOdd"});
 }
     
 }
